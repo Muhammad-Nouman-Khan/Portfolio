425115e76d7a3a9a932588a01a4dc6d46bf2e674
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2024-06-22 23:33:55 +0500

    final commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,20 +10,16 @@ import {
   reactjs,
   redux,
   tailwind,
-  nodejs,
-  mongodb,
   git,
-  figma,
-  docker,
-  meta,
   starbucks,
   tesla,
   shopify,
-  carrent,
-  jobit,
-  tripguide,
   threejs,
 } from "../assets";
+import instagram from "../assets/instagram.png";
+import brainwave from "../assets/brainwave.png";
+import gemini from "../assets/gemini.png";
+import hoobank from "../assets/hoobank.png";
 
 export const navLinks = [
   {
@@ -46,15 +42,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Front-End Web Dev",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "C++",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Firebase",
     icon: creator,
   },
 ];
@@ -89,14 +85,6 @@ const technologies = [
     icon: tailwind,
   },
   {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
     name: "Three JS",
     icon: threejs,
   },
@@ -104,23 +92,15 @@ const technologies = [
     name: "git",
     icon: git,
   },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
 ];
 
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
+    company_name: "",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -128,43 +108,30 @@ const experiences = [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
+
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
+    title: "C++",
+    company_name: "",
     icon: shopify,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Proficient in Object-Oriented Programming (OOP) and Data Structures and Algorithms (DSA) using C++.",
+      "Designed and implemented efficient algorithms and data structures.",
+      " Utilized OOP principles to create modular and maintainable code.",
     ],
   },
   {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
+    title: "Front-End Web Dev",
+    company_name: "",
+    icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Experienced in creating responsive and interactive web applications using HTML, CSS, and JavaScript.",
+      "Developed user-friendly interfaces with React and Tailwind css.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Implemented dynamic functionalities and single-page applications (SPAs) using JavaScript and React.",
     ],
   },
 ];
@@ -198,58 +165,57 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Instagram Clone",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Instagram clone in which users can upload images, also includes like and comment functionality.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "firebase",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "redux",
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
+    image: instagram,
+    source_code_link: "https://github.com/Muhammad-Nouman-Khan/Instagram-clone",
   },
   {
-    name: "Job IT",
+    name: "Modern Website",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Modern Website built using React and Tailwind CSS, showcasing my knowledge of web development.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "tailwindcss",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "vite",
         color: "pink-text-gradient",
       },
     ],
-    image: jobit,
-    source_code_link: "https://github.com/",
+    image: brainwave,
+    source_code_link: "https://github.com/Muhammad-Nouman-Khan/BrainwaveClone",
   },
   {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    name: "HooBank",
+    description: "Modern website using react.js",
     tags: [
       {
-        name: "nextjs",
+        name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "vite",
         color: "green-text-gradient",
       },
       {
@@ -257,8 +223,29 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: hoobank,
+    source_code_link:
+      "https://github.com/Muhammad-Nouman-Khan/ReactJSProjects/tree/main/HooBank",
+  },
+  {
+    name: "Gemini",
+    description: "Google Gemini clone using react.js",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gemini,
+    source_code_link: "https://github.com/Muhammad-Nouman-Khan/GeminiClone",
   },
 ];
 
